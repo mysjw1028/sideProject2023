cdce4d9e371e5ea49a61f188c9eb9dbe50289ebf
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-12-13 11:14:16 +0900

    뷰 초기 세팅 및 화면 테스트 완료

---
+++ src/main/java/site/metacoding/firstapp/web/ViewTestController.java
@@ -0,0 +1,38 @@
+package site.metacoding.firstapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewTestController {
+
+    @GetMapping("/user/loginForm") // 화면 출력되는지 확인 완료
+    public String 로그인페이지() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/joinForm") // 화면 출력되는지 확인 완료
+    public String 회원가입페이지() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/mainForm") // 화면 출력되는지 확인 완료
+    public String 메인페이지() {
+        return "mainForm";
+    }
+
+    @GetMapping("/categorylistForm") // 화면 출력되는지 확인 완료
+    public String 카테고리리스트() {
+        return "category/listForm";
+    }
+
+    @GetMapping("/category/updateForm") // 화면 출력되는지 확인 완료
+    public String 카테고리업데이트() {
+        return "category/updateForm";
+    }
+
+    @GetMapping("/category/writeForm") // 화면 출력되는지 확인 완료
+    public String 카테고리글쓰기() {
+        return "category/writeForm";
+    }
+}
