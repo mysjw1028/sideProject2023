b16c06062e50d89cda14dcee3ffd8d544b45bd58
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-12-26 12:26:41 +0900

    사진 전송 후 ok 나오는지 확인 완료

--- src/main/java/site/metacoding/firstapp/web/PostController.java
+++ src/main/java/site/metacoding/firstapp/web/PostController.java
@@ -1,12 +1,20 @@
 package site.metacoding.firstapp.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.firstapp.domain.post.Post;
@@ -19,6 +27,7 @@ import site.metacoding.firstapp.web.dto.post.PostUpdateRespDto;
 public class PostController {
 
     private final PostDao postDao;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/post/listForm/{userId}")
     public String 내블로그(Model model) {
@@ -78,4 +87,25 @@ public class PostController {
         return "redirect:/";
     }
 
+    @GetMapping("/uploadhome")
+    public String index() {
+        return "test";
+    }
+
+    @PostMapping("/upload")
+    public @ResponseBody String create(@RequestPart MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        System.out.println("fileName : " + fileName);
+        // 사진을 받았는데, file을 받았는데 (DB에 넣을지, 서버 하드디스크에 기록할 지!!)
+        String filePath = "c:\\upload\\" + fileName;
+        System.out.println("filePath : " + filePath);
+
+        File dest = new File(filePath);
+        try {
+            Files.copy(file.getInputStream(), dest.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ok";
+    }
 }
